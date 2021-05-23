47ff88339e2591ede73617e83759d1902d81b64a
GiovaneKayser <giovane.kayser16@gmail.com>
2021-05-23 14:44:51 -0300

    ajuste

--- app/Controllers/Http/ShopsController.ts
+++ app/Controllers/Http/ShopsController.ts
@@ -4,27 +4,27 @@ export default class ShopsController {
   async index({ view }) {
     const itens = [
       {
-        imageUrl: '/assets/img/camisa.png',
+        imageUrl: '/img/camisa.png',
         price: 100.00,
         name:'Camisa vermelha'
       },
       {
-        imageUrl: '/assets/img/calca.png',
+        imageUrl: '/img/calca.png',
         price: 120.00,
         name:'Calça azul'
       },
       {
-        imageUrl: '/assets/img/bota.png',
+        imageUrl: '/img/bota.png',
         price: 1000.00,
         name:'Bota'
       },
       {
-        imageUrl: '/assets/img/bola.png',
+        imageUrl: '/img/bola.png',
         price:5000.00,
         name:'Bola vermelha'
       },
       {
-        imageUrl: '/assets/img/comida.png',
+        imageUrl: '/img/comida.png',
         price: 10000.00,
         name:'Bolinho'
       }
